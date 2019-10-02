a042b4e84f8a6556a85af8e78a1e5afb27898172
Jeffrey Evans <jeffrey_evans@tnc.org>
2019-10-02 16:50:36 -0600

    updates

--- R/area.graph.statistics.R
+++ R/area.graph.statistics.R
@@ -1,8 +1,12 @@
 #' @title Statistics for edges (lines) based on a defined scale (area).
-#' @description Samples rasters for each edge and calculates specified statistics
+#' @description Samples rasters for each edge and calculates specified 
+#'              statistics for buffer distance
 #'   
 #' @param ... Parameters to be passed to the modern version of the function
 #'   
+#' @note Please note that this function has been deprecated, please use graph.statistics 
+#'       with the buffer argument.
+#'
 #' @export
 area.graph.statistics <- function(...) {
      .Deprecated("area.graph.statistics", package="GeNetIt", 
