e1a6734d5f1d04a43f4f830334db0476990b813e
Jeffrey Evans <jeffrey_evans@tnc.org>
2022-05-23 15:48:04 -0600

    bug fix

--- R/dmatrix.df.R
+++ R/dmatrix.df.R
@@ -16,15 +16,16 @@
 #'         Melanie A. Murphy <melanie.murphy@@uwyo.edu>
 #'
 #' @examples 
-#'  library(sp)
-#'  pts <- cbind(x=runif(15, 480933, 504250), y=runif(15, 4479433, 4535122))
-#'    pts <- SpatialPointsDataFrame(pts, 
-#'             data.frame(ID=paste("ob",1:nrow(pts),sep="")))
+#'  library(sf)
+#'  pts <- data.frame(ID=paste0("ob",1:15), x=runif(15, 480933, 504250), 
+#'                    y=runif(15, 4479433, 4535122))
+#'    pts <- st_as_sf(pts, coords = c("x", "y"), 
+#'                    crs = 32611, agr = "constant") 
 #'  
 #'  # Create distance matrix  
-#'  dm <- spDists(pts, pts)  
-#'    colnames(dm) <- pts@@data[,"ID"] 
-#'    rownames(dm) <- pts@@data[,"ID"]
+#'  dm <- units::drop_units(st_distance(pts))  
+#'    colnames(dm) <- pts$ID 
+#'    rownames(dm) <- pts$ID
 #'  
 #'  # Coerce to data.frame with TO and FROM ID's and associated distance
 #'  dm.df <- dmatrix.df(dm)
@@ -32,15 +33,29 @@
 #'
 #' @export
 dmatrix.df <- function(x, rm.diag = TRUE) {
+  if( nrow(x) != ncol(x))
+    stop("Matrix is not symmetrical")
   if(rm.diag == TRUE) { diag(x) <- NA }
-    varnames = names(dimnames(x))
-    values <- as.vector(x)
-    dn <- dimnames(x)
-    char <- sapply(dn, is.character)
+  if(is.null(rownames(x)) && is.null(colnames(x)))
+    stop("Either rows or columns need names")  
+  if(is.null(rownames(x))) {
+    rownames(x) <- colnames(x)  
+  }
+  if(is.null(colnames(x))) {
+    colnames(x) <- rownames(x)  
+  }
+  if(any(rownames(x) != colnames(x))){
+    message("names do not match; defaulting to column names") 
+	rownames(x) <- colnames(x) 
+  }	
+    varnames = list(colnames(x),colnames(x))
+      values <- as.vector(x)
+      dn <- dimnames(x)
+        char <- sapply(dn, is.character)
     dn[char] <- lapply(dn[char], utils::type.convert, as.is=TRUE)
     indices <- do.call(expand.grid, dn)
-      names(indices) <- varnames
+        names(indices) <- c("from","to")
       indices <- data.frame(indices, distance = values)
-      indices <- stats::na.omit(indices)	
+    indices <- stats::na.omit(indices)	
   return( indices )
 }
